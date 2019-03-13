014d916dfb3c5e44ca82a5d7323e133684e0a694
yanli98 <yan.li.18@ucl.ac.uk>
2019-03-13 11:16:40 -0400

    modify the file to take values from the form and send them to the server

--- uploadData.js
+++ uploadData.js
@@ -1,3 +1,7 @@
 function startDataUpload() {
-alert ("start data upload");
+var name = document.getElementById("name").value;
+var surname = document.getElementById("surname").value;
+var module = document.getElementById("module").value;
+var postString = "name="+name +"&surname="+surname+"&module="+module;
+alert (postString);
 }
