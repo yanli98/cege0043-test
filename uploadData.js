323bb2b0a1564a372f12e0a02bfe9fc4e1a8de07
yanli98 <yan.li.18@ucl.ac.uk>
2019-03-13 11:50:13 -0400

    managing the select box, checkbox and radio buttons

--- uploadData.js
+++ uploadData.js
@@ -3,6 +3,28 @@ var name = document.getElementById("name").value;
 var surname = document.getElementById("surname").value;
 var module = document.getElementById("module").value;
 var postString = "name="+name +"&surname="+surname+"&module="+module;
+
+// now get the checkbox values - separate them with a | so that they can be
+// split later on if necessary
+var checkString = "";
+for (var i = 1;i< 5;i++){
+if (document.getElementById("check"+i).checked === true) {
+checkString = checkString +
+document.getElementById("check"+i).value + "||"
+}
+}
+postString = postString + "&modulelist="+checkString;
+// now get the radio button values
+if (document.getElementById("morning").checked) {
+postString=postString+"&lecturetime=morning";
+}
+if (document.getElementById("afternoon").checked) {
+postString=postString+"&lecturetime=afternoon";
+}
+// now get the select box values
+var language = document.getElementById("languageselectbox").value;
+postString = postString + "&language="+language;
+
 alert (postString);
 processData(postString);
 }
@@ -14,7 +36,7 @@ client.open('POST','http://developer.cege.ucl.ac.uk:30287/reflectData',true);
 client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 client.onreadystatechange = dataUploaded;
 client.send(postString);
-p}
+}
 
 // create the code to wait for the response from the data server, and process the
 response once it is received
