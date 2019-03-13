1d67a8c8ee9aca2dcb7a1b84944040f4f3654e0d
yanli98 <yan.li.18@ucl.ac.uk>
2019-03-13 11:36:52 -0400

    use an AJAX request to send the data to the server

--- uploadData.js
+++ uploadData.js
@@ -4,4 +4,25 @@ var surname = document.getElementById("surname").value;
 var module = document.getElementById("module").value;
 var postString = "name="+name +"&surname="+surname+"&module="+module;
 alert (postString);
+processData(postString);
+}
+
+var client; // the global variable that holds the request
+function processData(postString) {
+client = new XMLHttpRequest();
+client.open('POST','http://developer.cege.ucl.ac.uk:30287/reflectData',true);
+client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+client.onreadystatechange = dataUploaded;
+client.send(postString);
+p}
+
+// create the code to wait for the response from the data server, and process the
+response once it is received
+function dataUploaded() {
+// this function listens out for the server to say that the data is ready - i.e.
+has state 4
+if (client.readyState == 4) {
+// change the DIV to show the response
+document.getElementById("dataUploadResult").innerHTML = client.responseText;
+}
 }
